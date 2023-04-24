3f0363b8341e5c2fbee6beec95b2a9c72b8a2f07
Eduardo de Oliveira Liria <dudev2001@gmail.com>
2023-04-24 01:26:14 -0300

    update

---
+++ script.js
@@ -0,0 +1,52 @@
+let gerador = document.querySelector('.gerador');
+let senhaGerada = document.querySelector('.senha-gerada');
+let copiarSenha = document.querySelector('.copy-js');
+let inputQuantidade = document.querySelector('.inputQuantidade');
+let modal = document.querySelector(".modal")
+let closeModal = document.querySelector('.btn-fechar-modal')
+
+
+function gerarSenha(tamanho) {
+  let senhaAleatoria = '';
+  let caracteres =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < tamanho; i++) {
+    senhaAleatoria += caracteres.charAt(
+      Math.floor(Math.random() * caracteres.length),
+    );
+  }
+  return senhaAleatoria;
+}
+
+function mostrarSenha() {
+  if (inputQuantidade.value === '') {
+    alert('Favor, digite uma quantidade!');
+  } else if (inputQuantidade.value > 10) {
+    alert('Excedeu o numeros maximo de caracateres, minino: 10');
+  } else {
+    senhaGerada.innerHTML = gerarSenha(inputQuantidade.value);
+  }
+
+  inputQuantidade.value = '';
+  inputQuantidade.focus();
+}
+
+
+function copyPassword() {
+  if(senhaGerada.innerHTML === 'Senha Gerada'){
+    alert("Favor, gere uma quantidade!")
+  }else{
+    navigator.clipboard.writeText(senhaGerada.innerHTML);
+    modal.classList.add('modal-ativo')
+  }
+}
+
+
+function fecharModal(){
+  modal.classList.remove('modal-ativo')
+}
+
+// eventos
+gerador.addEventListener('click', mostrarSenha);
+copiarSenha.addEventListener('click', copyPassword);
+closeModal.addEventListener("click", fecharModal)
